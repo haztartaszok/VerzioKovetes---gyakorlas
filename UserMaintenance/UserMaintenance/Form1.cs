1f4565410434a702f6801923ee178c77033e02ad
haztartaszok <haztartaszok@gmail.com>
2026-04-08 17:33:45 +0000

    1. létrehozva: entities mappa, user.cs, resources. 2. form1 dizájnban vannak objektumok feladat szerint 5. feladatig jutottam

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Properties;
 
 namespace UserMaintenance
 {
@@ -15,6 +16,10 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
+            lblLastName.Text = Resources.LastName; // label1
+            lblFirstName.Text = Resources.FirstName; // label2
+            btnAdd.Text = Resources.Add; // button1
         }
+
     }
 }
